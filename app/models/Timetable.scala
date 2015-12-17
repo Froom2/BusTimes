4014088efe915efc900ee8e4341d16a84e0f35ed
Froom2 <nicole@avison.co.uk>
2015-12-17 20:24:07 +0000

    Some updates

--- app/models/Timetable.scala
+++ app/models/Timetable.scala
@@ -1,22 +1,19 @@
 package models
 
-import org.joda.time.{LocalTime, LocalDateTime}
+import org.joda.time.LocalTime
 
 case class Timetable (weekday: String, timetableTime: LocalTime, timetableBus: String)  // make this so it takes day of week, time, busNumber
 
 object Timetable {
 
-//  def makeTimes(currentTime: LocalDateTime, day: Int, hour: Int, minutes: Int): LocalDateTime = {
-//    // get the year and month from the current time and add it to the hour and minutes
-//  }
-
   val timeList = Set (
+    Timetable("Weekday", new LocalTime(11, 0), "63"),
     Timetable("Weekday", new LocalTime(12, 0), "62"),
     Timetable("Weekday", new LocalTime(13, 0), "63")
   )
 
   def findNext(currentTime: LocalTime): Timetable = {
-    val thing: Set[Timetable] = timeList.filter(_.timetableTime isAfter(currentTime))
+    val thing: Set[Timetable] = timeList.filter(_.timetableTime isAfter currentTime)
     thing.head
   }
 
--- test/BusTimesSpec.scala
+++ test/BusTimesSpec.scala
@@ -13,19 +13,23 @@ class BusTimesSpec extends PlaySpec {
 
 
     "return the time of a bus at a specific time" in {
-      BusTimes.time(new LocalDateTime(2015,2,13,1,14)) must be ("2015-02-13T01:14:00.000")
+      BusTimes.time(new LocalDateTime(2015,2,13,1,14)) must be(
+        "2015-02-13T01:14:00.000")
     }
 
     "return the time of the bus if the given time is before the bus time" in {
-      BusTimes.nextBus(new LocalDateTime(2015,2,13,1,12)) must be ("Fri 13 Feb, 2015, 01:14")
+      BusTimes.nextBus(new LocalDateTime(2015,2,13,1,12)) must be(
+        "Fri 13 Feb, 2015, 01:14")
     }
 
     "return an error message if there are no bus times after the given time" in {
-      BusTimes.nextBus(new LocalDateTime(2015,2,13,1,20)) must be ("No more busses!")
+      BusTimes.nextBus(new LocalDateTime(2015,2,13,1,20)) must be(
+        "No more busses!")
     }
 
     "return the time of the next bus when there are two times" in {
-      Timetable.findNext(new LocalTime(12, 30)) must be (Timetable("Weekday", new LocalTime(13, 0), "63"))
+      Timetable.findNext(new LocalTime(12, 30)) must be(
+        Timetable("Weekday", new LocalTime(13, 0), "63"))
     }
 
     "return the time of the next bus on weekday" in {
@@ -33,7 +37,8 @@ class BusTimesSpec extends PlaySpec {
       val currentDay = DateTime.now.toString("EEEEEEEEE")
 
       println(currentDay)
-      Timetable.findNext(new LocalTime(12, 30)) must be (Timetable("Weekday", new LocalTime(13, 0), "63"))
+      Timetable.findNext(new LocalTime(12, 30)) must be(
+        Timetable("Weekday", new LocalTime(13, 0), "63"))
     }
 
   }
