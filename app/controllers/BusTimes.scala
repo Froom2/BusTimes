3222dbe5ff5cabf9613dc099f309f07d4043c7e4
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-01-28 13:35:38 +0000

    tidying

--- app/controllers/BusTimes.scala
+++ app/controllers/BusTimes.scala
@@ -2,37 +2,23 @@
 package controllers
 
 import models.{Bus, TimetableList}
-import org.joda.time.format.DateTimeFormat
-import org.joda.time.{LocalTime, LocalDateTime, DateTime}
-import play.api._
-import play.api.mvc._
-import views.html.main
+import org.joda.time.{LocalTime, LocalDateTime}
+import play.api.mvc.Controller
+import views.html.page
+
 
 class BusTimes extends Controller {
 
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
-  def time(busTime: LocalDateTime) = {
-
-    val giveTime = busTime.toString
-    giveTime
-
-  }
-
-  def nextBus(currentTime: LocalDateTime) = {
+  def nextBus = {
     val next: Either[String, Bus] = TimetableList.findNext(LocalDateTime.now.dayOfWeek.getAsText, LocalTime.now)
 
     next match {
-      case Right(bus) => Ok(main(bus))
+      case Right(bus) => Ok(page(bus))
       case _ => InternalServerError
     }
   }
 
 }
-
 
 
 object BusTimes extends BusTimes
